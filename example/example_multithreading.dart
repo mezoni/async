7cd5c1eef2df27e32ec6f929ae0e5f73ff31438c
andrew <andrew.mezoni@gmail.com>
2013-05-19 21:04:42 +0600

    commit

---
+++ example/example_multithreading.dart
@@ -0,0 +1,67 @@
+import 'package:async/async.dart';
+
+void main() {
+  var example = new Example();
+  example.run();
+}
+
+class Example {
+  void run() {
+    var ce = new CancelEvent();
+    var timeout = 4000;
+    new Async.delay(timeout, () {
+      print('Canceling long running tasks after $timeout ms...');
+      ce.set();
+    }, cancelEvent: ce);
+
+    var numberOfTasks = 2;
+    for(var i = 0; i < numberOfTasks; i++) {
+      var name = "LongTask #$i";
+      var times = (i + 1) * 2;
+      new Async.run(new LongRunningTask(name, times), cancelEvent: ce)
+      .continueWith((ant) {
+        var status = ant.status;
+        var arrow = '<===============';
+        if(status == AsyncStatus.DONE) {
+          print('$name complete, result: ${ant.result} $arrow');
+        } else {
+          print('$name $status $arrow');
+        }
+      });
+    }
+  }
+}
+
+class LongRunningTask extends Runnable {
+  final String name;
+  final int times;
+
+  LongRunningTask(this.name, this.times) {
+    if(times == null || times < 0) {
+      throw new ArgumentError('times: $times');
+    }
+  }
+
+  String run() {
+    for(var i = 0; i < times; i ++) {
+      doWork(i);
+    }
+
+    return '$times from $times';
+  }
+
+  int doWork(int i) {
+    print('Multithreading task: $name, $i from $times');
+    _simulate(1500);
+  }
+
+  void _simulate(int ms) {
+    var sw = new Stopwatch();
+    sw.start();
+    while(true) {
+      if(sw.elapsedMilliseconds > ms) {
+        break;
+      }
+    }
+  }
+}
---
+++ lib/src/runnable.dart
@@ -0,0 +1,5 @@
+part of async;
+
+abstract class Runnable<T> {
+  T run();
+}
