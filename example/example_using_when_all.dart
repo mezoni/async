a0751e54f052f0ad16d9464945d76880d7627d3e
andrew <andrew.mezoni@gmail.com>
2013-05-27 14:14:28 +0600

    commit

--- example/example_using_when_all.dart
+++ example/example_using_when_all.dart
@@ -9,33 +9,42 @@ void main() {
 
 class Example {
   void run() {
-    get100_500().then((_100_500) => print(_100_500));
-  }
-
-  Async<int> get100() {
-    return new Async(() => 100);
-  }
-
-  Async<int> get500() {
-    return new Async(() => 500);
-  }
-
-  Async<int> make100_500(int _100, int _500) {
-    return new Async<int>(() {
-      return _100 * 1000 + _500;
+    var sw = new Stopwatch();
+    sw.start();
+    var task1 = new Async.run(new SimpleParallelTask('Task1', 1));
+    var task2 = new Async.run(new SimpleParallelTask('Task2', 2));
+    var task3 = new Async.run(new SimpleParallelTask('Task3', 3));
+    Async.whenAll([task1, task2, task3])
+    .then((List results) {
+      sw.stop();
+      var ellapsed = sw.elapsedMilliseconds;
+      print('Results $results obtained from ${results.length} tasks in $ellapsed ms.');
     });
   }
+}
 
-  Async<int> get100_500() {
-    return new Async(() {
-      var current = Async.current;
-      var _100 = get100();
-      var _500 = get500();
-      Async.whenAll([_100, _500]).then((all) {
-        make100_500(all[0], all[1]).then((result) {
-          current.result = result;
-        });
-      });
-    });
+class SimpleParallelTask implements Runnable {
+  final int seconds;
+  final String name;
+
+  SimpleParallelTask(this.name, this.seconds);
+
+  int run() {
+    print('$name running (in blocking mode) for $seconds sec.');
+    loop(seconds);
+    print('$name finshed.');
+    return seconds * 2;
+  }
+
+  void loop(int seconds) {
+    var sw = new Stopwatch();
+    sw.start();
+    while(true) {
+      if(sw.elapsedMilliseconds / 1000 > seconds) {
+        break;
+      }
+    }
+
+    sw.stop();
   }
 }
